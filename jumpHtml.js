e1a1392918195079f7c842090bbac60689580818
Francesco Bedussi <francesco@francescobedussi.it>
2016-02-16 16:42:43 +0100

    Animazione versione HTML

--- jumpHtml.js
+++ jumpHtml.js
@@ -1,42 +1,34 @@
 (function(speed, heroThickness, heroHeight, jumpHeight){
-    var hero = document.getElementById('hero');
+    var hero = document.getElementById('hero'),
+        field = document.getElementById('field'),
         display = document.getElementById('display'),
         holesJumped = 0, gameOver = false, holes = [], jump = false;
     function game () {
         setTimeout(function(){
-            //var l = Math.random()*50;
-            //if ((!holes.length || holes[holes.length-1][0]+holes[holes.length-1][1]<width*0.8) && l > heroThickness) {holes.push([width, l]);}
-            //ctx.clearRect(0,0,width,height);
-            //ctx.fillStyle = "black";
-            //ctx.fillRect(0,height-heroThickness,width,heroThickness);
-            //display.innerHTML = holesJumped;
-            //ctx.fillStyle = "red";
-            //if (jump) {
-            //    ctx.fillRect(10,height-heroThickness-heroHeight-jumpHeight,heroThickness,heroHeight);
-            //    
-            //} else {
-            //    ctx.fillRect(10,height-heroThickness-heroHeight,heroThickness,heroHeight);    
-            //} 
-            //for(var i = 0; i < holes.length; i++){
-            //    if( !jump && holes[i][0] <= 10 && holes[i][0]+holes[i][1] >= 10+heroThickness){gameOver = true;}
-            //    ctx.fillStyle = "white";
-            //    ctx.fillRect(holes[i][0],height-heroThickness,holes[i][1],heroThickness);
-            //    if (holes[i][0]+holes[i][1] <= 0 ) {
-            //        holes.splice( i, 1);
-            //        i--;
-            //        holesJumped++;
-            //    } else {
-            //        holes[i][0] -= 5;
-            //    }
-            //}
-            //if (!gameOver) {
-            //    window.requestAnimationFrame(game);
-            //} else {
-            //    display.innerHTML += '<span>GAME OVER</span>';
-            //    ctx.clearRect(10,height-heroThickness-heroHeight,heroThickness,heroHeight);
-            //    ctx.fillStyle = "red";
-            //    ctx.fillRect(10,height-heroHeight,heroThickness,heroHeight);
-            //}  
+            display.innerHTML = holesJumped;
+            var l = Math.random()*20;
+            var holes = document.querySelectorAll('.hole');
+            if (!holes.length || holes[holes.length-1].getBoundingClientRect().right < window.innerWidth*0.8) {
+                var d = document.createElement("div");
+                d.className = 'hole';
+                d.style.width = l+'%';
+                field.appendChild(d);
+            }
+            
+            [].forEach.call(holes,function(hole){
+               hole.style.transform = 'translateX(' + (hole.getBoundingClientRect().right - window.innerWidth - 5) + 'px)';
+               
+                if (hole.getBoundingClientRect().left + hole.getBoundingClientRect().width < 0) {
+                    field.removeChild(hole);
+                    holesJumped++;
+                }
+            });
+            
+            if (!gameOver) {
+                window.requestAnimationFrame(game);
+            } else {
+                display.innerHTML += '<span>GAME OVER</span>';
+            }  
         },speed -=0.01);
     }
     document.addEventListener('keyup', function (e) {
