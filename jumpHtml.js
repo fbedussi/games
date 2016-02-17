543369399fc2f2bf1515dc7728c89fd79abcd45d
Francesco Bedussi <francesco@francescobedussi.it>
2016-02-17 15:05:19 +0100

    Gestione game over in HTML version

--- jumpHtml.js
+++ jumpHtml.js
@@ -1,14 +1,14 @@
-(function(speed, heroThickness, heroHeight, jumpHeight){
+(function(speed, jumpDuration, speedIncrement, holeMaxLength, holesGap){
     var hero = document.getElementById('hero'),
         field = document.getElementById('field'),
         display = document.getElementById('display'),
-        holesJumped = 0, gameOver = false, holes = [], jump = false;
-    function game () {
+        holesJumped = 0, gameOver = false;
+    (function game () {
         setTimeout(function(){
             display.innerHTML = holesJumped;
-            var l = Math.random()*20;
+            var l = Math.random()*holeMaxLength;
             var holes = document.querySelectorAll('.hole');
-            if (!holes.length || holes[holes.length-1].getBoundingClientRect().right < window.innerWidth*0.8) {
+            if (!holes.length || holes[holes.length-1].getBoundingClientRect().right < window.innerWidth*holesGap) {
                 var d = document.createElement("div");
                 d.className = 'hole';
                 d.style.width = l+'%';
@@ -16,24 +16,27 @@
             }
             
             [].forEach.call(holes,function(hole){
-               hole.style.transform = 'translateX(' + (hole.getBoundingClientRect().right - window.innerWidth - 5) + 'px)';
-               
+                hole.style.transform = 'translateX(' + (hole.getBoundingClientRect().right - window.innerWidth - 5) + 'px)';
+            
+                if (!hero.classList.contains('jump') && hole.getBoundingClientRect().left < hero.getBoundingClientRect().left && hole.getBoundingClientRect().right > hero.getBoundingClientRect().right) {
+                    gameOver = true;
+                }
+                
                 if (hole.getBoundingClientRect().left + hole.getBoundingClientRect().width < 0) {
                     field.removeChild(hole);
                     holesJumped++;
                 }
             });
             
-            if (!gameOver) {
-                window.requestAnimationFrame(game);
+            if (gameOver) {
+                field.classList.add('gameOver');
             } else {
-                display.innerHTML += '<span>GAME OVER</span>';
+                window.requestAnimationFrame(game);
             }  
-        },speed -=0.01);
-    }
+        },speed -=speedIncrement);
+    })()
     document.addEventListener('keyup', function (e) {
         hero.classList.add('jump');
-        setTimeout(function(){hero.classList.remove('jump');}, 300);
+        setTimeout(function(){hero.classList.remove('jump');}, jumpDuration);
     }, false);
-    game();
-})(33,20, 50, 50);
+})(33, 500, 0.01,15,0.8);
