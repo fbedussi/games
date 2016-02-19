4d8c7559e106018687afd3d7cac694d93b9a54f7
Francesco Bedussi <francesco@francescobedussi.it>
2016-02-19 18:17:00 +0100

    Display INIT

--- simon.js
+++ simon.js
@@ -4,12 +4,24 @@
     });
     
     var containerEl = document.getElementById('container');
-    
+    var display = document.getElementById('display');
+    var userSequence = [];
     containerEl.style.width = containerEl.getBoundingClientRect().height + 'px';
     
     function addElement(sequence) {
         sequence.push(Math.floor(Math.random()*4));
         return sequence;
+    }
+    
+    function signalEnd() {
+        display.classList.remove('hide');
+        setTimeout(function(){display.classList.add('hide');}, 1000);
+    }
+    
+    function getUserInput() {
+        document.querySelectorAll('light').addEventListener('click', function(e){
+            userSequence.push
+        });
     }
     
     sequence = addElement(sequence);
@@ -22,9 +34,12 @@
                 lights[sequence[sequencePos]].classList.remove('on');
                 
                 lights[sequence[sequencePos]].addEventListener('transitionend', function nextLight(){
-                    lights[sequence[sequencePos]].removeEventListener(nextLight);
+                    lights[sequence[sequencePos]].removeEventListener('transitionend');
                     if (sequencePos < sequence.length-1) {
                         lightOn(++sequencePos);
+                    } else {
+                        signalEnd();
+                        getUserInput();
                     }
                 });
             },lightDuration);
